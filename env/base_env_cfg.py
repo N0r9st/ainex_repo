03f0b1b869a6df5e65f328c61164e61045599976
Norest <sqidde@gmail.com>
2025-01-12 19:55:09 +0300

    rough terrain cfg

--- env/base_env_cfg.py
+++ env/base_env_cfg.py
@@ -28,60 +28,14 @@ import omni.isaac.lab_tasks.manager_based.locomotion.velocity.mdp as mdp
 ##
 # Pre-defined configs
 ##
-from omni.isaac.lab.terrains.config.rough import ROUGH_TERRAINS_CFG  # isort: skip
+from .rough_terrain_cfg import ROUGH_TERRAINS_CFG  # isort: skip
 
 
 ##
 # Scene definition
 ##
 
-
-# @configclass
-# class MySceneCfg(InteractiveSceneCfg):
-#     """Configuration for the terrain scene with a legged robot."""
-
-#     # ground terrain
-#     terrain = TerrainImporterCfg(
-#         prim_path="/World/ground",
-#         terrain_type="generator",
-#         terrain_generator=ROUGH_TERRAINS_CFG,
-#         max_init_terrain_level=5,
-#         collision_group=-1,
-#         physics_material=sim_utils.RigidBodyMaterialCfg(
-#             friction_combine_mode="multiply",
-#             restitution_combine_mode="multiply",
-#             static_friction=1.0,
-#             dynamic_friction=1.0,
-#         ),
-#         visual_material=sim_utils.MdlFileCfg(
-#             mdl_path=f"{ISAACLAB_NUCLEUS_DIR}/Materials/TilesMarbleSpiderWhiteBrickBondHoned/TilesMarbleSpiderWhiteBrickBondHoned.mdl",
-#             project_uvw=True,
-#             texture_scale=(0.25, 0.25),
-#         ),
-#         debug_vis=False,
-#     )
-#     # robots
-#     robot: ArticulationCfg = MISSING
-#     # sensors
-#     height_scanner = RayCasterCfg(
-#         prim_path="{ENV_REGEX_NS}/Robot/base",
-#         offset=RayCasterCfg.OffsetCfg(pos=(0.0, 0.0, 20.0)),
-#         attach_yaw_only=True,
-#         pattern_cfg=patterns.GridPatternCfg(resolution=0.1, size=[1.6, 1.0]),
-#         debug_vis=False,
-#         mesh_prim_paths=["/World/ground"],
-#     )
-#     contact_forces = ContactSensorCfg(prim_path="{ENV_REGEX_NS}/Robot/.*", history_length=3, track_air_time=True)
-#     # lights
-#     sky_light = AssetBaseCfg(
-#         prim_path="/World/skyLight",
-#         spawn=sim_utils.DomeLightCfg(
-#             intensity=750.0,
-#             texture_file=f"{ISAAC_NUCLEUS_DIR}/Materials/Textures/Skies/PolyHaven/kloofendal_43d_clear_puresky_4k.hdr",
-#         ),
-#     )
-
-from ainex_cfg import AINEX_CFG, joint_names  # isort:skip
+from ainex_cfg import AINEX_COLLISION_CFG as AINEX_CFG, joint_names  # isort:skip
 
 
 @configclass
